42fdebec9242502af17b29c8468deb539db4bfd5
Megha Kaul <megha.aa12@gmail.com>
2026-06-03 10:24:25 -0400

    Add build info to footer (version, commit, build time, pipeline)

--- src/app/profile/footer/footer.component.ts
+++ src/app/profile/footer/footer.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {BuildInformationToken} from '../../services/build-information';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  providers: [{provide: BuildInformationToken, useValue: environment.build}]
 })
-export class FooterComponent {}
+export class FooterComponent {
+  buildInformation = inject(BuildInformationToken);
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ import {MockContactService} from '../app/services/contact/mock-contact.service';
 export const environment = {
   production: true,
   ipInfo: MockContactService,
+  build: {version: 'VERSION', commit: 'CI_COMMIT_SHORT_SHA', buildTime: 'BUILD_TIME', buildPipeline: 'CI_PIPELINE_ID'}
 };
